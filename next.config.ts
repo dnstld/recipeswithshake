c443d5872f68da30faa8f59d3063e87d48e29d35
Denis Toledo <contato@denistoledo.com.br>
2025-05-18 16:07:55 +0200

    add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  basePath: "/espacogitoledo",
+  assetPrefix: "/espacogitoledo",
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
